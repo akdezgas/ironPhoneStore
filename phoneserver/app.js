9c3ce335c85dc1b432bdac0d6491e24918b0bdd0
akdezgas <andreivelazquez94@gmail.com>
2018-03-21 17:53:54 -0600

    added public to heroku

--- phoneserver/app.js
+++ phoneserver/app.js
@@ -40,6 +40,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/phone',phoneController)
 
+
+app.all('/*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
